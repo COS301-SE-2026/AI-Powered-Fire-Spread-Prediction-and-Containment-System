1cfba1c5dd665008526004c7dbc97d431bce9c43
Axility <ryanmatlynn6@gmail.com>
2026-05-19 02:06:38 +0200

    sidebar for firefighter dashboard

---
+++ app/frontend/src/pages/firefighterDashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SidebarLayout } from '../components/firefighter/sidebar';
+
+export default function firefighterDashboard () {
+    return(
+        <div className="h-screen">
+            <SidebarLayout />
+        </div>
+    )
+}
