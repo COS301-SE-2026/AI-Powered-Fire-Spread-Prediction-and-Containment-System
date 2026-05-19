e4dba0d13502acea92220d16e30ae5c063ad51b3
Axility <ryanmatlynn6@gmail.com>
2026-05-19 17:46:36 +0200

    Correctly positioned the quick actions under map to the right

--- app/frontend/src/pages/firefighterDashboard.tsx
+++ app/frontend/src/pages/firefighterDashboard.tsx
@@ -29,25 +29,40 @@ export default function FirefighterDashboard() {
                 </header>                
 
                 {/* Main Grid container */}
-                <div className="grid grid-cols-1 xl:grid-cols-12 gap-6 flex-1 min-h-0">
-                    <div className="xl:col-span-8 rounded-2xl bg-carbon-side/40 border border-carbon-card backdrop-blur-sm flex flex-col overflow-hidden relative shadow-2xl shadow-black/20 min-h-[500px]">
-                        
-                        {/* Header overlay on map */}
-                        <div className="p-4 border-b border-carbon-card bg-carbon-bg/50 backdrop-blur-md absolute top-0 w-full z-10 flex justify-between items-center">
-                            <span className="font-bold text-m tracking-wide text-neutral/80">LIVE FIRE MAP</span>
+                <div className="grid grid-cols-1 xl:grid-cols-12 gap-4 flex-1 min-h-0">
+
+                    <div className="xl:col-span-8 flex flex-col gap-4 min-h-0">
+                        {/* Map */}
+                        <div className="rounded-2xl bg-carbon-side/40 border border-carbon-card backdrop-blur-sm flex flex-col overflow-hidden relative shadow-2xl shadow-black/20 min-h-[500px]">
+                            <div className="p-4 border-b border-carbon-card bg-carbon-bg/50 backdrop-blur-md absolute top-0 w-full z-10 flex justify-between items-center">
+                                <span className="font-bold text-m tracking-wide text-neutral/80">LIVE FIRE MAP</span>
+                            </div>
+                            <div className="flex-1 w-full h-full pt-[53px]"> 
+                                <FireMap />
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-2 xl-gird-cols-6 gap-2 flex-1 min-h-0">
+                            <div>
+                                <h2 className="text-xs font-bold tracking-widest text-neutral/50 uppercase mb-3">
+                                    Weather Statistics
+                                </h2>
+                                
+                            </div>
+                            <div>
+                                <h2 className="text-xs font-bold tracking-widest text-neutral/50 uppercase mb-3">
+                                    Quick Actions
+                                </h2>
+                                <QuickActions />
+                            </div>
                         </div>
                         
-                        {/* map */}
-                        <div className="flex-1 w-full h-full pt-[53px]"> 
-                            <FireMap />
-                        </div>
+
                     </div>
 
-                    {/* Right side control panel */}
-                    <div className="xl:col-span-4 flex flex-col gap-6 overflow-y-auto">
-                        <QuickActions />
+                    {/* Right Column */}
+                    <div className="xl:col-span-4 flex flex-col gap-4 overflow-y-auto">
+                        
                     </div>
-                    
                 </div>
             </div> 
         </SidebarLayout>
