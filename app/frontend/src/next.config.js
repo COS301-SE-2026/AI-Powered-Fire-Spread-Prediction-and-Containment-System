ac43fe9b602ee70382369a1bb00613e3d23845a8
Megglez <megan.laisa24@gmail.com>
2026-05-22 05:54:20 +0200

    eslint

--- app/frontend/src/next.config.js
+++ app/frontend/src/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ['mapbox-gl', 'react-map-gl'],
+      eslint: {
+        ignoreDuringBuilds: true,
+    },
     async rewrites() {
         return[
             {
