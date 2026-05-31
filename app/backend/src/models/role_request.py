44813ae6a2c6a5955680dc4e1c06d92be9f141cd
Axil1ty <ryanmatlynn6@gmail.com>
2026-05-31 20:25:31 +0200

    Added enums for backend, added pydantic schemas for role_requests and users, changed table models to use enums and also added changes where needed in certain columns

--- app/backend/src/models/role_request.py
+++ app/backend/src/models/role_request.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Enum
 from sqlalchemy.orm import relationship
+from src.enums.role_request_status import RequestStatus
+from src.enums.user_role import UserRole
 from datetime import datetime, timezone
 from src.db import Base
 
@@ -8,9 +10,11 @@ class RoleRequest(Base):
 
     request_id = Column(String, primary_key=True)
     user_id = Column(String, ForeignKey("users.id"), nullable=False)
-    request_role = Column(String, default="admin")
-    current_role = Column(String, nullable=False)
-    status = Column(String, default="pending", nullable=False)
+    requested_role = Column(Enum(UserRole), default=UserRole.admin, nullable=False)
+    current_role = Column(Enum(UserRole), nullable=False)
+    status = Column(Enum(RequestStatus), default=RequestStatus.pending, nullable=False)
     created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
     reviewed_by = Column(String, ForeignKey("users.id"), nullable=True)
     reviewed_at = Column(DateTime, nullable=True)
+    user = relationship("User", foreign_keys=[user_id], back_populates="role_requests")
+    reviewer = relationship("User", foreign_keys=[reviewed_by])
---
+++ app/backend/src/schemas/user.py
@@ -0,0 +1,25 @@
+from src.enums.user_role import UserRole
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    name:str
+    surname:str
+    email:str
+    id_number:str
+    license_number:Optional[str] = None
+    role:UserRole = UserRole.user
+
+class UserResponse(BaseModel):
+    id:str
+    name:str
+    surname:str
+    email:str
+    role:UserRole
+    created_at:datetime
+    is_active:bool
+    class Config:
+        from_attributes = True
+
+
