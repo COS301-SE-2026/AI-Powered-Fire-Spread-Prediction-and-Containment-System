8661f86bc9dcb6cbf59c871c1adc0b864e7efe13
JanriduToit <u23632730@tuks.co.za>
2026-06-01 21:07:11 +0200

    refactored colors

--- app/frontend/src/pages/_app.tsx
+++ app/frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import '../styles/brand_style.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
