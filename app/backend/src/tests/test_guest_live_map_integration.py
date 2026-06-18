fa5872149fd689da3fc539c9d48ef455a4666731
keyseringe <u23544563@tuks.co.za>
2026-06-18 20:17:07 +0200

    integration tests are written. Is being skipped, because PostGIS isn't set up for testing yet.

--- app/backend/src/tests/test_guest_live_map_integration.py
+++ app/backend/src/tests/test_guest_live_map_integration.py
@@ -1,35 +1,36 @@
-# import pytest
-# from geoalchemy2.elements import WKTElement
-# from models import FireReportModel, ReportStatus
+import pytest
+from geoalchemy2.elements import WKTElement
+from models import FireReportModel, ReportStatus
 
-# def test_guest_fire_map_integration(client, db):
-#     """Validate data retrieval for guest live map view"""
+@pytest.mark.skip(reason="PostGIS not configured for testing yet.")
+def test_guest_fire_map_integration(client, db):
+    """Validate data retrieval for guest live map view"""
 
-#     #mock record into test db
-#     mock_fire = FireReportModel(
-#         reference_number="FR-2026-100",
-#         user_id="usr_01",
-#         location_text="Place 1",
-#         description="Fake fire",
-#         location_geom=WKTElement("POINT(28.2293 -25.7479)", srid=4326),
-#         boundary_radius_km=2,
-#         status=ReportStatus.verified,
-#         status_index=2
-#     )
+    #mock record into test db
+    mock_fire = FireReportModel(
+        reference_number="FR-2026-100",
+        user_id="usr_01",
+        location_text="Place 1",
+        description="Fake fire",
+        location_geom=WKTElement("POINT(28.2293 -25.7479)", srid=4326),
+        boundary_radius_km=2,
+        status=ReportStatus.verified,
+        status_index=2
+    )
 
-#     db.add(mock_fire)
-#     db.commit()
+    db.add(mock_fire)
+    db.commit()
 
-#     response = client.get("api/reports/public")
+    response = client.get("api/reports/public")
 
-#     assert response.status_code == 200, "Since it is public unautherised shouldn't be returned"
+    assert response.status_code == 200, "Since it is public unautherised shouldn't be returned"
 
-#     data = response.json()
-#     assert isinstance(data, list), "Expect a list of fire reports"
-#     assert len(data) >= 1, "At least one active fire"
+    data = response.json()
+    assert isinstance(data, list), "Expect a list of fire reports"
+    assert len(data) >= 1, "At least one active fire"
 
-#     #fake fire just created
-#     guest_fire = next((item for item in data if item["reference_number"] == "FR-2026-100"), None)
-#     assert guest_fire is not None, "Failed to get the mock fire just created"
+    #fake fire just created
+    guest_fire = next((item for item in data if item["reference_number"] == "FR-2026-100"), None)
+    assert guest_fire is not None, "Failed to get the mock fire just created"
 
-#     assert "user_id" not in guest_fire, "Sensitive data leaked to public"
+    assert "user_id" not in guest_fire, "Sensitive data leaked to public"
