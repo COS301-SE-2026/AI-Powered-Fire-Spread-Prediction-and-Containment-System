277b432df1b543d5e2c0cb57d73aa4ca57ccf767
Axil1ty <ryanmatlynn6@gmail.com>
2026-06-24 20:15:16 +0200

    Added functional testing for firefighter page to test rendering of objects

--- app/frontend/src/playwright.config.ts
+++ app/frontend/src/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
-    testDir: './e2e',
+    testDir: './testing',
     use: {
         baseURL: 'http://localhost:3000',
     },
--- app/frontend/src/testing/firefighter/firefighterDashboard.spec.ts
+++ app/frontend/src/testing/firefighter/firefighterDashboard.spec.ts
@@ -3,23 +3,39 @@ import { test, expect } from '@playwright/test'
 test.describe('Firefighter Dashboard - live fire map', () => {
 
     test.beforeEach(async({ page }) =>{
-        await page.goto('/firefighterDashboard');
+        await page.goto('/firefighter/firefighterDashboard');
     });
 
-    test('mape stats overlay is visible', async({ page }) => {
-        await expect(page.getByText('ACTIVE FIRES')).toBeVisible();
-        await expect(page.getByText('NEAREST')).toBeVisible();
-        await expect(page.getByText('UNVERIFIED REPORTS')).toBeVisible();
-    });
+    test('Main page renders', async ({page}) =>{
+        await expect(page.getByRole('main')).toBeVisible();
+    })
 
     test('nearby reports section renders', async({ page }) => {
         await expect(page.getByText('Nearby Reports')).toBeVisible();
         await expect(page.getByText('Pretoria West')).toBeVisible();
     });
 
-    test('only verified fires are shown in nearby fires', async({ page }) => {
-        await expect(page.getByText('Active')).toBeVisible();
-        await expect(page.getByText('Pending')).toBeVisible();
-        await expect(page.getByText('Contained')).toBeVisible();
+    test('Map renders', async ({ page }) => {
+        await expect(page.getByRole('region', { name: 'Map' })).toBeVisible();      
+    })
+
+    test('All quick action map overlay stats are rendered', async ({page}) => {
+        await expect(page.getByText('3', { exact: true })).toBeVisible();
+        await page.getByText('2.7 km').click();
+        await page.getByText('9', { exact: true }).click();
+    })
+
+    test('All quick action buttons are rendered', async ({page}) => {
+        await page.getByRole('button', { name: 'Unit position View team on map' }).click();
+        await page.getByRole('button', { name: 'Report a fire New fire' }).click();
+        await page.getByRole('button', { name: 'Spread Simulation View AI' }).click();
+        await page.getByRole('button', { name: 'Log containment line Draw' }).click();
+    })
+
+    test('All environment variables are rendered', async ({page}) => {
+        await expect(page.locator('div').filter({ hasText: /^38°CTemperature$/ }).first()).toBeVisible();
+        await expect(page.locator('div').filter({ hasText: /^18 km\/hWind NW$/ }).first()).toBeVisible();
+        await expect(page.locator('div').filter({ hasText: /^HighFire Danger$/ }).first()).toBeVisible();
+        await expect(page.locator('div').filter({ hasText: /^32%Humidity$/ }).first()).toBeVisible();
     })
 });
