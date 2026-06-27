d607b4724b567d5d9ef4903e5bd3c03e40f0b61c
JanriduToit <u23632730@tuks.co.za>
2026-06-27 23:18:28 +0200

    fixed sonarcloud issues and deleted mockdata file

--- app/backend/src/routes/admin/fire_reports.py
+++ app/backend/src/routes/admin/fire_reports.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Annotated
 from db import get_db
 from schemas.fire_report import FireReportDetailResponse, FireReportMapResponse
 from services.users import fire_report
@@ -8,24 +8,18 @@ from enums.report_status import ReportStatus
 
 router = APIRouter(prefix="/api/admin", tags=["Admin"])
 
+dbSession = Annotated[Session, Depends(get_db)]
+
 @router.get("/reported-fires", response_model=List[FireReportMapResponse])
-def get_reported_fires(db:Session = Depends(get_db)):
-    try:
-        return fire_report.get_fire_reports(db)
-    except ValueError as e:
-        raise HTTPException(status_code=404, detail=str(e))
+def get_reported_fires(db: dbSession):
+    return fire_report.get_fire_reports(db)
 
 @router.get("/reported-fires/{report_id}", response_model=FireReportDetailResponse)
-def get_fire_report_id(report_id: str, db: Session = Depends(get_db)):
-    try:
-        return fire_report.get_fire_report_by_id(report_id, db)
-    except ValueError as e:
-        raise HTTPException(status_code=404, detail=str(e))
+def get_fire_report_id(report_id: str, db: dbSession):
+    return fire_report.get_fire_report_by_id(report_id, db)
 
 @router.patch("/reported-fires/{report_id}/status", response_model=FireReportDetailResponse)
-def status_change(report_id: str, status: ReportStatus, db:Session = Depends(get_db)):
-    try:
-        return fire_report.status_change(report_id, status, db)
-    except ValueError as e:
-        raise HTTPException(status_code=404, detail=str(e))
+def status_change(report_id: str, status: ReportStatus, db:dbSession):
+    return fire_report.status_change(report_id, status, db)
+
 
