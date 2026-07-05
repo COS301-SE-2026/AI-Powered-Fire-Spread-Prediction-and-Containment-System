299e5f1e14af10595906c1c944d7edcaa5855f74
keyseringe <u23544563@tuks.co.za>
2026-07-05 07:39:39 +0200

    updated auth.py in src

--- app/backend/src/auth.py
+++ app/backend/src/auth.py
@@ -46,4 +46,12 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     
     return payload
 
-#moet miskien nog een add vir admin spesifiek, maar nou te moeg om te dink
+#authorisation
+async def get_current_admin_user(current_user: dict = Depends(get_current_user)):
+    """Dependency ensures the current user has an Admin privilage"""
+    if current_user.get("role") != "Admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail = "Access denied: Admin privileges needed"
+        )
+    return current_user
