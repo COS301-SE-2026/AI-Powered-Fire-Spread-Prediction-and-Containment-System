9e1f0932c8c782860828190b944744d06412b679
JanriduToit <u23632730@tuks.co.za>
2026-07-08 22:43:52 +0200

    created types for colour section

---
+++ app/frontend/src/types/brandStyle.tsx
@@ -0,0 +1,12 @@
+export interface ColourToken {
+    name: string;
+    hex: string;
+    usage:string;
+    reason:string;
+}
+
+export interface ColourPairing {
+    label: string;
+    fg: string;
+    bg: string;
+}
