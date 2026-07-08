b88f73eae03c665a4fdcbc946261309b2a370484
Megglez <megan.laisa24@gmail.com>
2026-07-08 20:23:56 +0200

    user sidePanelRight start

---
+++ app/frontend/src/components/users/sidePanelRight.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const COL_SPAN_CLASSES: Record<number, string> = {
+    3: 'xl:col-span-3',
+    4: 'xl:col-span-4',
+    5: 'xl:col-span-5',
+    6: 'xl:col-span-6',
+}
