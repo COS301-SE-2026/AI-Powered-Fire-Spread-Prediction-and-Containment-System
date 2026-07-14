5b9cff808126d87964511c010ca67e33248dd7b8
JanriduToit <u23632730@tuks.co.za>
2026-07-14 16:59:21 +0200

    created adminReportFire to work with the admin sidebar

---
+++ app/frontend/src/pages/admin/adminReportFire.tsx
@@ -0,0 +1,10 @@
+import { AdminSideBarLayout } from '../../components/admin/adminSidebar';
+import ReportPage from '../../components/reportfire/report';
+
+export default function AdminReportFire() {
+    return (
+        <AdminSideBarLayout>
+            <ReportPage/>
+        </AdminSideBarLayout>
+    );
+}
