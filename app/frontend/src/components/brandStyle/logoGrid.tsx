07c56b50a0d6a34a8c7d79e8be1ada88cac9156a
JanriduToit <u23632730@tuks.co.za>
2026-07-15 13:39:36 +0200

    created the logo grid

---
+++ app/frontend/src/components/brandStyle/logoGrid.tsx
@@ -0,0 +1,12 @@
+import { LogoCard } from "./logoCard";
+
+export function LogoGrid() {
+    return (
+        <div className="grid grid-cols-2 gap-3">
+            <LogoCard src="/images/logo-large.png" filename="logo-large.png" note="Wordmark + icon (expanded sidebar)" imgHeight="h-35"/>
+            <LogoCard src="/images/logo-small.png" filename="logo-small.png" note="Icon only (collapsed sidebar)" imgHeight="h-35"/>
+            <LogoCard src="/images/logo-monochrome.png" filename="logo-monochrome.png" note="Single colour (print/watermark use)" imgHeight="h-35"/>
+            <LogoCard src="/images/logo-inverse.png" filename="logo-inverse.png" note="White backgrond (light mode or emails)" imgHeight="h-105"/>  
+        </div>
+    );
+}
