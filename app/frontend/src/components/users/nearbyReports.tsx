31d1c9e0818c0f82214f647e9665794b1868428e
Axil1ty <ryanmatlynn6@gmail.com>
2026-07-16 16:49:05 +0200

    added a new nearby fires component for users and guests now busy connecting it to the frontend

---
+++ app/frontend/src/components/users/nearbyReports.tsx
@@ -0,0 +1,36 @@
+import { ChevronRight } from "lucide-react";
+import { useState } from "react";
+
+interface NearbyFire{
+    readonly location_text: string;
+    readonly distance: number;
+    readonly time_ago: string;
+    readonly status: string;
+}
+
+interface NearbyFireReports{
+    readonly nearby_fires: NearbyFire[];
+}
+
+export function NearbyReports({nearby_fires}: NearbyFireReports) {
+    const verified_fires = nearby_fires.filter((fire) => fire.status === 'verified');
+    if(verified_fires.length === 0){
+        return(
+            <div className="h-full flex items-center justify-center p-4">
+                <p className="text-xs opacity-50">No nearby fires</p>
+            </div>
+        )
+    }
+    return(
+        <div className="h-full overflow-y-auto flex flex-col p-2">
+        {verified_fires.map((fire, i) => (
+            <div key={i} className="flex items-center justify-between rounded-lg px-3 py-2.5 border border-carbon-stroke hover:border-ignite mb-2 hover:bg-carbon-card/50 cursor-pointer transition-colors">
+                <div>
+                    <p className="font-semibold text-sm">{fire.location_text}</p>
+                    <p className="text-xs opacity-50">{fire.distance} km · {fire.time_ago}</p>
+                </div>
+        </div>
+        ))}
+        </div>
+    );
+}
