0219b715831a4d2d091e5a6de2e67d5540ca7e48
Axil1ty <ryanmatlynn6@gmail.com>
2026-07-20 21:54:39 +0200

    the base usage of the dca not modified yet doing research and going through the source code to see how it needs to change for our use case

--- app/backend/src/ai/dca.py
+++ app/backend/src/ai/dca.py
@@ -1,1 +1,9 @@
-# TODO: Later. 
+from pytorch import WildfireModel
+
+model = WildfireModel() # creates a model with default params and environment data
+model = model.cuda() # move model to gpu
+
+for _ in range(100): # runs model for 100 steps
+    model.compute()
+
+    # reference is the study done for pytorch by MIT this is just a base modal is gonna be modified to not use the other variables but to use the ignition from the xgboost
