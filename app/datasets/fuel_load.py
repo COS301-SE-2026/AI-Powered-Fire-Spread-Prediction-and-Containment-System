94118aff3705af50ee82441dd360ca00cb436ccc
keyseringe <u23544563@tuks.co.za>
2026-07-21 12:31:23 +0200

    dataset too big

--- app/datasets/fuel_load.py
+++ app/datasets/fuel_load.py
@@ -1,2 +1,19 @@
-import ee
-ee.Initialize()
+import numpy as np
+import rasterio
+from rasterio.enums import Resampling
+
+#use 0.5 for values for now, will change when get 
+FUEL_BASE_WEIGHTS = {
+    10: 0.50# Tree cover
+    # Shrubland
+    # Grassland
+    # Cropland
+    # Built-up
+    # Bare / sparse vegetation
+    # Snow and ice
+    # Permanent water bodies
+    # Herbaceous wetland
+    # Mangroves
+    # Moss and lichen
+
+}
