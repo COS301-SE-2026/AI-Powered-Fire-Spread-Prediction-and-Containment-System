d16d8eaa22bc4273651af47f0798cf740b0660cc
Axil1ty <ryanmatlynn6@gmail.com>
2026-07-22 10:50:26 +0200

    added a function to return the environment data as needed by the DCA/pytorchfire model

--- app/backend/src/ai/simulation.py
+++ app/backend/src/ai/simulation.py
@@ -23,3 +23,19 @@ def pick_ignition_points(p_ignite: np.ndarray, n_points: int = 1, rng=None) -> n
     mask.flat[idx] = True # this is an iterator view over the 2D array, that maps the flattend postitions back onto the original 2D grid position
     return mask
 
+def build_env_data(weather_np: dict, static_np: dict, initial_ignition_mask: np.ndarray) -> dict:
+
+    u = torch.from_numpy(weather_np["wind_u"]).float()
+    v = torch.from_numpy(weather_np["wind_v"]).float()
+
+    wind_velocity = torch.sqrt(u**2 + v**2)
+    wind_towards_direction = torch.rad2deg(torch.atan2(v,u)) % 360
+
+    return {
+        "p_veg": torch.from_numpy(static_np["fuel_load"]).float(),
+        "p_den": torch.from_numpy(static_np["dryness"]).float(),
+        "wind_velocity": wind_velocity,
+        "wind_towards_direction": wind_towards_direction,
+        "slope": torch.zeros((*wind_velocity.shape, 3, 3), dtype=torch.float32),
+        "initial_ignition": torch.from_numpy(initial_ignition_mask).bool(),
+    }
