59e361ab9cb75bb91a6db32f96cff7eb92b1dd7e
Axil1ty <ryanmatlynn6@gmail.com>
2026-07-27 14:31:45 +0200

    feat: added test-minio to test docker yaml
    refactor:removed the hardcoded env values in the db.py

--- app/backend/src/db.py
+++ app/backend/src/db.py
@@ -4,13 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 load_dotenv()
 
-DB_HOST = os.environ.get("DB_HOST", "postgres")
-DB_PORT = os.environ.get("DB_PORT", "5432")
-DB_NAME = os.environ.get("POSTGRES_DB", "fireaway_db")
-DB_USER = os.environ.get("POSTGRES_USER", "fireaway_admin")
-DB_PASS = os.environ.get("POSTGRES_PASSWORD", "supersecretpassword")
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
