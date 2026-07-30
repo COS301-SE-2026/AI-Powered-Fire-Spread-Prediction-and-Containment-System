d8ea1ac188f24c9c68d5be3fa13935eed9c1051f
Axil1ty <ryanmatlynn6@gmail.com>
2026-07-31 01:40:39 +0200

    removed coverage in cicd its not working and flagged false flags from sonar

--- gpu_worker/worker.py
+++ gpu_worker/worker.py
@@ -22,8 +22,8 @@ log = logging.getLogger(
 )  # Logs go to local systemd journal only (journalctl -u fire-worker)
 
 # Where systemd unit's monitoring checks for liveness
-#NOSONAR
-HEARTBEAT_FILE = Path(os.environ.get("HEARTBEAT_FILE", "/tmp/gpu_worker_heartbeat"))
+
+HEARTBEAT_FILE = Path(os.environ.get("HEARTBEAT_FILE", "/tmp/gpu_worker_heartbeat")) # NOSONAR
 
 # SQS long-polling wait time
 WAIT_TIME_SECONDS = 20
