e321caa30a48eaa616ed3711b2c11666a3cf0188
MarcoD1357 <marcodewit45@gmail.com>
2026-07-30 19:03:18 +0200

    Fix cicd issues 1

--- app/frontend/src/testing/guests/guest-map-e2e.spec.ts
+++ app/frontend/src/testing/guests/guest-map-e2e.spec.ts
@@ -4,7 +4,7 @@ test('test', async ({ page }) => {
   await page.getByRole('img', { name: 'Fire Spread Prediction Logo' }).click();
   await page.getByRole('button', { name: 'Sign in as Guest' }).click();
   //checks the marker
-  await expect(page.locator('.relative.inline-flex').first()).toBeVisible();
+  await expect(page.locator('div:nth-child(9) > .relative.flex > .relative')).toBeVisible();
   //checks the animation
   await expect(page.locator('.animate-ping').first()).toBeVisible();
   //checks the radius (probably going to become a check for the points of a polygon object)
