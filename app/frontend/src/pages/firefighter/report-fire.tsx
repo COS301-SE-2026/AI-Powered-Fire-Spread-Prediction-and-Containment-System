d0277517868c92b6023ce6eb907499b21078935d
JanriduToit <u23632730@tuks.co.za>
2026-07-31 00:50:46 +0200

    fixed firefighter sidebar to have report a fire and the button navigates to that page

---
+++ app/frontend/src/pages/firefighter/report-fire.tsx
@@ -0,0 +1,10 @@
+import { FirefighterSideBar } from '../../components/firefighter/firefighterSidebar';
+import ReportPage from '../../components/reportfire/report';
+
+export default function FirefighterReportFire() {
+    return (
+        <FirefighterSideBar>
+            <ReportPage/>
+        </FirefighterSideBar>
+    );
+}
