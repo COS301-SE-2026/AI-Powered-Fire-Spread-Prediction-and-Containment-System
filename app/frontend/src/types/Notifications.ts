93b211129adc0e1330b075460ca04a5b322d7763
JanriduToit <u23632730@tuks.co.za>
2026-08-12 12:07:55 +0200

    feature: created Notification type to use

---
+++ app/frontend/src/types/Notifications.ts
@@ -0,0 +1,12 @@
+export type NotificationType = 'alert' | 'update';
+export type NotificationSeverity = 'low' | 'moderate' | 'high' | 'extreme';
+
+export type FireNotification = Readonly <{
+    id: string;
+    fireLocation: string;
+    distance: number;
+    type: NotificationType;
+    mapDeepLink: string;
+    time: string;
+    read: boolean;
+}>;
