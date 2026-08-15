87f482b2acdaa3533233799e10ca154e125c1bc3
JanriduToit <u23632730@tuks.co.za>
2026-08-15 23:35:12 +0200

    feature: created notification toast

--- app/frontend/src/pages/_app.tsx
+++ app/frontend/src/pages/_app.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import 'mapbox-gl/dist/mapbox-gl.css'
-import { NotificationsProvider } from "../hooks/useNotification";
+import { NotificationsProvider, useNotifications } from "../hooks/useNotification";
+import { NotificationToast } from '../components/notification/NotificationToast';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NotificationsProvider>
       <Component {...pageProps} />
+      <GlobalToast />
     </NotificationsProvider>
+  );
+}
+
+function GlobalToast() {
+  const { activeToast, dismissToast } = useNotifications();
+  if (!activeToast) return null;
+  return (
+    <div className="toast toast-top toast-end z-100">
+      <NotificationToast notification={activeToast} onDismiss={dismissToast} />
+    </div>
   );
 }
 
