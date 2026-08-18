e179491d745b4c21c9d84040098470adc87a0f10
JanriduToit <u23632730@tuks.co.za>
2026-08-18 16:00:24 +0200

    fix: fromat time issue

--- app/frontend/src/components/shared/FormatDate.ts
+++ app/frontend/src/components/shared/FormatDate.ts
@@ -1,6 +1,6 @@
 export function FormatDate(isoString: string): string {
     const date = new Date(isoString);
-    const datePart = date.toLocaleDateString('en-ZA', { day: 'numeric', month: 'short' });
-    const timePart = date.toLocaleTimeString('en-ZA', { hour: '2-digit', minute: '2-digit' });
+    const datePart = date.toLocaleDateString('en-ZA', { day: 'numeric', month: 'short', timeZone: 'Africa/Johannesburg'});
+    const timePart = date.toLocaleTimeString('en-ZA', { hour: '2-digit', minute: '2-digit', timeZone: 'Africa/Johannesburg' });
     return `${datePart} | ${timePart}`;
 }
