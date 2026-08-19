7e84d38680b15386fd10f26474c0b153e34edf5b
Megglez <megan.laisa24@gmail.com>
2026-08-19 19:05:55 +0200

    feat(model): notification model

---
+++ app/backend/src/models/notification.py
@@ -0,0 +1,26 @@
+import uuid
+from datetime import datatime, timezone
+
+from sqlalchemy import Boolean, Column, DateTime, Enum, Float, ForeignKey, String
+from sqlalchemy.orm import relationship
+
+from db import Base
+from enums.notification_type import NotificationType
+from enums.severity import Severity
+
+class Notification(Base):
+    __table__ = "notifications"
+    
+    id = Column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
+    user_id = Column(String, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
+    fire_report_id = Column(String, ForeignKey("fire_reports.id", ondelete="CASCADE"), nullable=False, index=True)
+    type = Column(Enum(NotificationType), nullable=False)
+    Severity = Column(Enum(Severity), nullable=False)
+    message = Column(String, nullable=False)
+    fire_location = Column(String, nullable=False)   # snapshots - frozen at creation time so history doesn't shift if fire report or user location changes later
+    distance = Column(Float, nullable=False)   
+    read = Column(Boolean, default=False, nullable=False)
+    time = Column(DateTime, nullable=False, default=lambda: datatime.now(timezone.utc))
+    
+    user = relationship("User")
+    fire_report = relationship("FireReports")
