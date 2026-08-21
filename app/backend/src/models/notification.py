63f716e0bb4e89dd9a56e0c5ad16d7ba57f01770
Megglez <megan.laisa24@gmail.com>
2026-08-21 19:19:20 +0200

    fix(notifications model): spelling mistake

--- app/backend/src/models/notification.py
+++ app/backend/src/models/notification.py
@@ -9,18 +9,18 @@ from enums.notification_type import NotificationType
 from enums.severity import Severity
 
 class Notification(Base):
-    __table__ = "notifications"
+    __tablename__ = "notifications"
     
     id = Column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     user_id = Column(String, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
     fire_report_id = Column(String, ForeignKey("fire_reports.id", ondelete="CASCADE"), nullable=False, index=True)
     type = Column(Enum(NotificationType), nullable=False)
-    Severity = Column(Enum(Severity), nullable=False)
+    severity = Column(Enum(Severity), nullable=False)
     message = Column(String, nullable=False)
     fire_location = Column(String, nullable=False)   # snapshots - frozen at creation time so history doesn't shift if fire report or user location changes later
     distance = Column(Float, nullable=False)   
     read = Column(Boolean, default=False, nullable=False)
-    time = Column(DateTime, nullable=False, default=lambda: datatime.now(timezone.utc))
+    time = Column(DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
     
     user = relationship("User")
     fire_report = relationship("FireReports")
