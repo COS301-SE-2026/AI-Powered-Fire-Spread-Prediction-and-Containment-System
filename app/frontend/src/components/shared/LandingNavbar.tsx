9e5a364d378df7d67f58d2db5896c8d63d7ec8a8
Axil1ty <ryanmatlynn6@gmail.com>
2026-08-27 17:18:57 +0200

    feat: added styling to the navbar aswell as correct section names

--- app/frontend/src/components/shared/LandingNavbar.tsx
+++ app/frontend/src/components/shared/LandingNavbar.tsx
@@ -1,6 +1,7 @@
 export function LandingNavbar() {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="p-8">
+    <div className="navbar bg-base-100 shadow-sm bg-carbon-side/80 backdrop-blur-md rounded-xl border border-carbon-stroke">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -22,55 +23,50 @@ export function LandingNavbar() {
           </div>
           <ul
             tabIndex={-1}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-display uppercase tracking-wide text-lg"
           >
             <li>
-              <a>Item 1</a>
+              <a className='text-text-muted hover:text-text-primary'>Home</a>
             </li>
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <a className='text-text-muted hover:text-text-primary'>About</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a className='text-text-muted hover:text-text-primary'>How it works</a>
+            </li>
+            <li>
+              <a className='text-text-muted hover:text-text-primary'>Features</a>
+            </li>
+            <li>
+              <a className='text-text-muted hover:text-text-primary'>Meet the team</a>
             </li>
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal px-1 font-display uppercase tracking-wide text-lg">
           <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 w-40 z-1">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+              <a className='text-text-muted hover:text-text-primary'>Home</a>
+            </li>
+            <li>
+              <a className='text-text-muted hover:text-text-primary'>About</a>
+            </li>
+            <li>
+              <a className='text-text-muted hover:text-text-primary'>How it works</a>
+            </li>
+            <li>
+              <a className='text-text-muted hover:text-text-primary'>Features</a>
+            </li>
+            <li>
+              <a className='text-text-muted hover:text-text-primary'>Meet the team</a>
+            </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className='btn btn-primary uppercase text-text-primary font-display text-lg'>Get Started</a>
       </div>
+    </div>
     </div>
   );
 }
