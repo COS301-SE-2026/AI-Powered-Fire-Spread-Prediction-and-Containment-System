2c455ad786f21d7edf6c246a46fa1880cab45d10
keyseringe <u23544563@tuks.co.za>
2026-09-01 14:15:07 +0200

    fix handle higher load

--- app/backend/db.py
+++ app/backend/db.py
@@ -8,7 +8,15 @@ load_dotenv()
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=20,
+    max_overflow=40,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True,
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
