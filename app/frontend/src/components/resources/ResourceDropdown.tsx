6af0f05f194951eab77e66ba64480bb45ebeba80
JanriduToit <u23632730@tuks.co.za>
2026-09-18 14:04:13 +0200

    update: updated that the other has name and capacity input

--- app/frontend/src/components/resources/ResourceDropdown.tsx
+++ app/frontend/src/components/resources/ResourceDropdown.tsx
@@ -17,17 +17,28 @@ const RESOURCE_TYPES:{ value: Resource; label: string } [] = [
 interface ResourceDropdownProps{
     readonly value: Resource;
     readonly other: string;
+    readonly otherCapacity: string;
     readonly onChange: (value: Resource) => void;
     readonly onChangeOther: (value: string) => void;
+    readonly onChangeOtherCapacity: (value: string) => void;
     readonly error?: string;
 }
 
-export function ResourceDropdown({ value, other, onChange, onChangeOther, error }: ResourceDropdownProps) {
+export function ResourceDropdown({ value, other, otherCapacity, onChange, onChangeOther, onChangeOtherCapacity, error }: ResourceDropdownProps) {
     return (
         <>
             <Dropdown id="resource-type" value={value} options={RESOURCE_TYPES} onChange={onChange} error={error} />
             {value === 'other' && (
-                <input type="text" placeholder='Describe the resource' className='input w-full bg-carbon-input border-carbon-stroke mt-2' value={other} onChange={(e) => onChangeOther(e.target.value)} />
+                <div className='grid grid-cols-2 gap-3 w-full mb-1 mt-2'>
+                    <div className='w-full'>
+                        <h4 className='p-1'>Resource Name</h4>
+                        <input type="text" placeholder='e.g. Diesel generator' className='input input-bordered w-full bg-surface-input border-carbon-stroke focus:outline-primary' value={other} onChange={(e) => onChangeOther(e.target.value)} />
+                    </div>
+                    <div className='w-full'>
+                        <h4 className='p-1'>Capacity Unit</h4>
+                        <input type="text" placeholder='e.g. liters, boxes, units' className='input input-bordered w-full bg-surface-input border-carbon-stroke focus:outline-primary' value={otherCapacity} onChange={(e) => onChangeOtherCapacity(e.target.value)} />
+                    </div>
+                </div>
             )}
         </>
     );
