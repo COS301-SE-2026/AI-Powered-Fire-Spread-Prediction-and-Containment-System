87ca5fec81f6df2ee132f065cd6500b5ceec4850
JanriduToit <u23632730@tuks.co.za>
2026-09-19 12:22:34 +0200

    feat: added Register Resources page to firefighters

---
+++ app/frontend/src/pages/firefighter/RegisterResource.tsx
@@ -0,0 +1,10 @@
+import { FirefighterSideBar } from '../../components/firefighter/FirefighterSidebar';
+import RegisterResource from '../../components/resources/RegisterResourcePage';
+
+export default function RegisteredReportFire() {
+  return (
+    <FirefighterSideBar>
+      <RegisterResource />
+    </FirefighterSideBar>
+  );
+}
