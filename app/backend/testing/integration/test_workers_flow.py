1a0d4d6c93a740acb6a0c9b84f06784209d51525
keyseringe <u23544563@tuks.co.za>
2026-09-20 20:34:21 +0200

    fix commented out, cause didn't want to commit that test in the first place.

--- app/backend/testing/integration/test_workers_flow.py
+++ app/backend/testing/integration/test_workers_flow.py
@@ -1,10 +1,10 @@
-import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
+# import pytest
+# from fastapi.testclient import TestClient
+# from sqlalchemy.orm import Session
 
-from main import app
-from db import get_db
-from app.backend.src.dependencies.auth import create_access_token
-from app.backend.src.models.users import User
-from app.backend.src.models.workers import WorkerNode
-from app.backend.src.routes.workers import
+# from main import app
+# from db import get_db
+# from app.backend.src.dependencies.auth import create_access_token
+# from app.backend.src.models.users import User
+# from app.backend.src.models.workers import WorkerNode
+# from app.backend.src.routes.workers import
