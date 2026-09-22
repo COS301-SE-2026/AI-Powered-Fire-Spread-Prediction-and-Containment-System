1b94e431bbb5ee6cd62f6e50be6948a2b4880432
JanriduToit <u23632730@tuks.co.za>
2026-09-22 16:11:49 +0200

    feat: made the sidebar item to navigate to the page on users

--- app/frontend/src/components/users/UserItems.tsx
+++ app/frontend/src/components/users/UserItems.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, Flame, House, MessageCircleWarning, MessagesSquare } from 'lucide-react';
+import { Map, Flame, House, Cpu, MessageCircleWarning, MessagesSquare } from 'lucide-react';
 import { NavLink } from '../layout/NavLink';
 
 export function UserItems() {
@@ -7,6 +7,7 @@ export function UserItems() {
     <>
       <NavLink icon={House} label="Home" href="/users/live-map" />
       <NavLink icon={Flame} label="Report a Fire" href="/users/report-fire" />
+      <NavLink icon={Cpu} label="Volunteer yout GPU" href="/users/GpuVolunteer" />
       {/* <NavLink icon={Map} label="Fire Simulation" href="/users/simulation" />
       <NavLink icon={MessageCircleWarning} label="Notifications" href="/users/under-construction" />
       <NavLink icon={MessagesSquare} label="Community" href="/users/under-construction" /> */}
