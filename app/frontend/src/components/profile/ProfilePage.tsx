ab3340d3321fa4e1c082501495428a80ab59b7bc
JanriduToit <u23632730@tuks.co.za>
2026-09-26 14:10:00 +0200

    update: wired My firer reports card together

--- app/frontend/src/components/profile/ProfilePage.tsx
+++ app/frontend/src/components/profile/ProfilePage.tsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import type { ReportStatus } from '../../types/Report';
 import { useUserInfo } from '../../hooks/useUserInfo';
+import { useMyReportedFires } from '../../hooks/useMyReportedFires';
+import { MyFireReportsTable } from '../../components/profile/FireReportsTable';
+import { ReportFilterTabs } from '../../components/admin/reportFilter';
+import { SearchBar } from '../../components/admin/searchBar';
 
 export default function ProfilePage(){
     const { user, isLoading} = useUserInfo();
+    const { reports, loading: reportsLoading, error: reportsError } = useMyReportedFires();
+    const [filter, setFilter] = useState<'All' | ReportStatus>('All');
+    const [search, setSearch] = useState('');
 
     let name = "";
     let initial = "";
@@ -15,6 +24,13 @@ export default function ProfilePage(){
         name = "Not signed in";
         initial = "?";
     }
+
+    const filteredReports = reports.filter(
+        (report) =>
+            report.location_text.toLowerCase().includes(search.toLowerCase()) ||
+            report.id.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
         <div className='p-6'>
             <div className='flex items-center gap-8 mt-6 mb-8 pb-8 bg-card'>
@@ -26,6 +42,28 @@ export default function ProfilePage(){
                     ) : null}
                 </div>
             </div>
+            {user ? (
+                <div className='w-full rounded-2xl border border-carbon-stroke p-4 flex flex-col'>
+                    <h2 className='uppercase mb-3'>
+                        My Fire Reports
+                    </h2>
+
+                    <div className='flex flex-col md:flex-row md:items-center md:justify-between gap-2 mb-3'>
+                        <SearchBar value={search} onChange={setSearch} placeholder='Search...' />
+                        <ReportFilterTabs filter={filter} onChange={setFilter} />
+                    </div>
+        
+                    {reportsError && <div>{reportsError}</div>}
+ 
+                    {reportsLoading ? (
+                    <div className="flex justify-center items-center min-h-[20vh]">
+                        <span className="loading loading-spinner loading-lg text-primary" />
+                    </div>
+                    ) : (
+                        <MyFireReportsTable reports={filteredReports} filter={filter} />
+                    )}
+                </div>
+            ) : null}
         </div>
     );
 }
